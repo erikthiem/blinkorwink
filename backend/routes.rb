95153f3354ae720abe262aed0e967d03e7bc10ea
Erik Thiem <erikthiem@gmail.com>
2017-01-04 20:01:56 -0600

    Fixed frontend relative folder path

--- backend/routes.rb
+++ backend/routes.rb
@@ -4,7 +4,7 @@ set :public_folder, File.dirname(__FILE__) + '/../frontend/'
 
 get '/' do
     content_type "text/html"
-    send_file File.expand_path('index.html', '../frontend/')
+    send_file File.expand_path('index.html', 'frontend/')
 end
 
 get '/helloworld' do
