20e80d22a01f68bc9c032611380fb9a9a8a134b7
Erik Thiem <erikthiem@gmail.com>
2017-01-08 21:56:25 -0600

    Added empty vue app to frontend

---
+++ frontend/js/vueApp.js
@@ -0,0 +1,17 @@
+var apiBaseUrl = "http://localhost:9393"
+
+var vueApp = new Vue({
+    
+    el: '#vueApp',
+    data: {
+
+    },
+
+    mounted: function() {
+
+    },
+
+    methods: {
+       
+    }
+})
