b8879bc94b378f680205bf628131e12a1d5deb14
Erik Thiem <erikthiem@gmail.com>
2017-01-22 17:42:04 -0600

    Hooked up basic ajax call to create new game'

--- frontend/js/vueApp.js
+++ frontend/js/vueApp.js
@@ -1,8 +1,11 @@
 var apiBaseUrl = "http://localhost:9393"
 
+var apiBaseUrl = ""
+
 var vueApp = new Vue({
     
     el: '#vueApp',
+
     data: {
         game_code: ""
     },
@@ -12,6 +15,25 @@ var vueApp = new Vue({
     },
 
     methods: {
-       
+        create: function(e) {
+            var self = this;
+
+			$.ajax({
+                url: apiBaseUrl + "/games",
+                contentType: "application/json",
+                method: "POST",
+                success: function(data) {
+                    console.log(data);
+                },
+                error: function(error) {
+                    console.log(error);
+                }
+            });
+
+        },
+        
+        join: function(e) {
+            alert('clicked join');
+        }
     }
 })
